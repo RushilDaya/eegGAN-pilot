03072de13a0d6c0cecdddb75ae4ced8251353d9b
RushilDaya <rushildayabme@gmail.com>
2019-11-01 16:39:23 +0200

    function to generated segment data

--- pickle2labelled.py
+++ pickle2labelled.py
@@ -6,8 +6,47 @@ import matplotlib.pyplot as plt
 
 def _trimContinguous(A):
     # returns only the first element per contigous batch in a larger list A
+
+    if len(A) == 0:
+        return []
+
+    trimedList = [A[0]]
+    runningIndex = A[0]
+    for idx in range(1,len(A)):
+        if A[idx] != A[idx-1]+1:
+            trimedList +=[ A[idx] ]
+        
+    return trimedList
+
+def _getsegments(electrodeChannels, freqMarkers, markerTrialStarts,markerTrialEnds,eventMarkerStarts,eventMarkerEnds):
+    # uses the obtained indices to split the dataset for the particular frequency markers considered
     
-    return A
+    allSegments = []
+
+    for freqMarker in freqMarkers:
+        trialStart = [item for item in markerTrialStarts if item > freqMarker ][0]
+        trialEnd   = [item for item in markerTrialEnds if item > freqMarker ][0]
+
+
+        filteredEventStarts = [item for item in eventMarkerStarts if (item > trialStart and item < trialEnd )]
+        filteredEventEnds = [item for item in eventMarkerEnds if (item > trialStart and item < trialEnd )]
+
+        if len(filteredEventEnds) != len(filteredEventEnds):
+            raise Exception
+
+        # we want the segments NOT corresponding to events
+        # hence the switch around here
+        segmentBeginings = [trialStart]+ filteredEventEnds
+        segmentEndings = filteredEventStarts + [trialEnd]
+
+        for idx in range(len(segmentBeginings)):
+            begining = segmentBeginings[idx] 
+            ending = segmentEndings[idx] 
+            segment = electrodeChannels[begining:ending,:]
+            allSegments += [segment]
+
+    return allSegments
+
 
 def pickle2labelled(picklePath, paradigm, reref=True, bandpass=True):
     # segments the raw data into sections labelled segments
@@ -65,17 +104,29 @@ def pickle2labelled(picklePath, paradigm, reref=True, bandpass=True):
     markerLocations11 = [i for i in range(len(markerChannel)) if markerChannel[i]==markers["trialPrior11"]]
     markerLocations13 = [i for i in range(len(markerChannel)) if markerChannel[i]==markers["trialPrior13"]]
     markerLocations15 = [i for i in range(len(markerChannel)) if markerChannel[i]==markers["trialPrior15"]]
-
     markerLocations11 = _trimContinguous(markerLocations11)
     markerLocations13 = _trimContinguous(markerLocations13)
     markerLocations15 = _trimContinguous(markerLocations15)
 
-    print(markerLocations11)
-    print(markerLocations13)
-    print(markerLocations15)
+    markerTrialStarts =[i for i in range(len(markerChannel)) if markerChannel[i]==markers["trialStart"]]
+    markerTrialEnds = [i for i in range(len(markerChannel)) if markerChannel[i]==markers["trialEnd"]]
+    markerTrialStarts = _trimContinguous(markerTrialStarts)
+    markerTrialEnds = _trimContinguous(markerTrialEnds)
 
-    return True
+    eventMarkerStarts = [i for i in range(len(markerChannel)) if markerChannel[i]==markers["eventStart"]]
+    eventMarkerEnds = [i for i in range(len(markerChannel)) if markerChannel[i]==markers["eventEnd"]]
+    eventMarkerStarts = _trimContinguous(eventMarkerStarts) 
+    eventMarkerEnds =  _trimContinguous(eventMarkerEnds)
+
+    labelledSet[11]= _getsegments(electrodeChannels, markerLocations11, markerTrialStarts,markerTrialEnds,eventMarkerStarts,eventMarkerEnds)
+    labelledSet[13]= _getsegments(electrodeChannels, markerLocations13, markerTrialStarts,markerTrialEnds,eventMarkerStarts,eventMarkerEnds)
+    labelledSet[15]= _getsegments(electrodeChannels, markerLocations15, markerTrialStarts,markerTrialEnds,eventMarkerStarts,eventMarkerEnds)
+    
+    return {
+        'data':labelledSet,
+        'sampleRate': sampleRate,
+        'electrodeLabels': electrodeList
+    }    
 
 if __name__ == '__main__':
-    exitCode = pickle2labelled('pickleData/subject_1/onoff.pickle','onoff')
-    print(exitCode)
+    exitCode = pickle2labelled('pickleData/subject_1/training.pickle','training')
